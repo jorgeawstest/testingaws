0aa9e25179294b3f3dd37c285458f44072685d38
pakjivega <pakjivega@gmail.com>
2020-12-19 23:30:27 +0100

    change name bucket2

--- testingawsiascode/src/main/resources/commands/deploy_S3.sh
+++ testingawsiascode/src/main/resources/commands/deploy_S3.sh
@@ -1,11 +1,11 @@
 aws cloudformation deploy --template-file ../awsformation/createS3.json --stack-name createS3stack
 
 wget https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv
-aws s3 cp titanic.csv s3://s3titanic/inputdata/titanic.csv
-aws s3 cp ../awsformation/titanicstepMachine.json s3://s3titanic/statemachine/titanicstepMachine.json
+aws s3 cp titanic.csv s3://testingaws-titanic/inputdata/titanic.csv
+aws s3 cp ../awsformation/titanicstepMachine.json s3://testingaws-titanic/statemachine/titanicstepMachine.json
 rm titanic.csv
 
 # http://www.fao.org/faostat/en/#home
 # wget http://fenixservices.fao.org/faostat/static/bulkdownloads/FAOSTAT.zip
-# aws s3 cp FAOSTAT.zip s3://s3titanic/inputdata/zip/FAOSTAT.zip
+# aws s3 cp FAOSTAT.zip s3://testingaws-faostat/inputdata/zip/FAOSTAT.zip
 
