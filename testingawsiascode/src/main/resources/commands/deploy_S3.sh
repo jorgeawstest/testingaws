95c42ba1248effb8e3981cc399d9f1eba80090b9
pakjivega <pakjivega@gmail.com>
2020-12-19 23:13:42 +0100

    remove titanic.csv

--- testingawsiascode/src/main/resources/commands/deploy_S3.sh
+++ testingawsiascode/src/main/resources/commands/deploy_S3.sh
@@ -3,6 +3,7 @@ aws cloudformation deploy --template-file ../awsformation/createS3.json --stack-
 wget https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv
 aws s3 cp titanic.csv s3://s3titanic/inputdata/titanic.csv
 aws s3 cp ../awsformation/titanicstepMachine.json s3://s3titanic/statemachine/titanicstepMachine.json
+rm titanic.csv
 
 # http://www.fao.org/faostat/en/#home
 wget http://fenixservices.fao.org/faostat/static/bulkdownloads/FAOSTAT.zip
