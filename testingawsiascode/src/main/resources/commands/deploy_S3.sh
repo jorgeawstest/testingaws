ca81210ddcc66218ffdb2074959c6d94343e7f61
pakjivega <pakjivega@gmail.com>
2020-12-19 23:07:01 +0100

    Include FAO S3 and zip

--- testingawsiascode/src/main/resources/commands/deploy_S3.sh
+++ testingawsiascode/src/main/resources/commands/deploy_S3.sh
@@ -3,3 +3,8 @@ aws cloudformation deploy --template-file ../awsformation/createS3.json --stack-
 wget https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv
 aws s3 cp titanic.csv s3://s3titanic/inputdata/titanic.csv
 aws s3 cp ../awsformation/titanicstepMachine.json s3://s3titanic/statemachine/titanicstepMachine.json
+
+# http://www.fao.org/faostat/en/#home
+wget http://fenixservices.fao.org/faostat/static/bulkdownloads/FAOSTAT.zip
+aws s3 cp FAOSTAT.zip s3://s3titanic/inputdata/zip/FAOSTAT.zip
+
