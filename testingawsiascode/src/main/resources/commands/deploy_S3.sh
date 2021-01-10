14d52ac59b184d70b4be26b1238fe5ddb58faada
pakjivega <pakjivega@gmail.com>
2021-01-10 20:48:10 +0100

    Include deploy in Codepipeline12

--- testingawsiascode/src/main/resources/commands/deploy_S3.sh
+++ testingawsiascode/src/main/resources/commands/deploy_S3.sh
@@ -3,6 +3,7 @@ aws cloudformation deploy --template-file ../awsformation/createS3.json --stack-
 wget https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv
 aws s3 cp titanic.csv s3://testingaws-titanic/inputdata/titanic.csv
 aws s3 cp ../awsformation/titanicstepMachine.json s3://testingaws-titanic/statemachine/titanicstepMachine.json
+aws s3 cp ../awsformation/faostepMachine.json s3://testingaws-faostat/statemachine/faostepMachine.json
 rm titanic.csv
 
 aws s3 cp ../awsformation/faodeploydata.sh s3://testingaws-faostat/bin/faodeploydata.sh
